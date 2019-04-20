9e176e3219e40e39958cf4aa5fc0d893ae4994d1
Jeff Andros <jeff@jeffandros.com>
2019-04-20 12:08:05 -0700

    actually doing what I said in the last commit

--- keyboards/idobo/keymaps/pequenininho/keymap.c
+++ keyboards/idobo/keymaps/pequenininho/keymap.c
@@ -15,9 +15,18 @@
  */
 #include QMK_KEYBOARD_H
 
-// Layer shorthand
-#define _QW 0
-#define _FN 1
+enum idobo_layers = {
+	_QWERTY,
+	_RAISE,
+	_LOWER,
+	_ADJUST
+}
+
+#define LOWER TT(_LOWER)
+#define RAISE TT(_RAISE)
+#define RALTD RALT_T(KC_DOWN)
+#define RGUIU RGUI_T(KC_UP)
+#define RCTLR RCTL_T(KC_RGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -35,12 +44,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
- [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
-   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F2,   KC_GRV,  KC_F5,   KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC, \
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS, KC_BSLS, KC_EQL,  KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_QUOT, \
-   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-   KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_LBRC,  KC_RBRC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL  \
+ [_QWERTY] = LAYOUT_ortho_5x15(
+   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                               KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS, KC_BSLS, KC_EQL,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
+   KC_LCTL, KC_LGUI, KC_LALT, KC_LOCK, LOWER,   KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_SPC,   RAISE,   KC_LEFT, RALTD,   RGUIU,   RCTLR  \
  ),
 
 /* FUNCTION Layer - removed the rgb hue saturation and vibrance for audio controls and added some functionality I use from other boards.
@@ -57,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
  
- [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
+ [_RAISE] = LAYOUT_ortho_5x15( 
    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
    KC_MSEL, KC_CALC, KC_UP  , _______, _______, KC_MNXT, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
    KC_MPRV, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , KC_MPLY, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______, \
