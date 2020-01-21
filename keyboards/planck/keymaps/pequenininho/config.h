d19a3fcee3bf27c29458f64358f2453953f3386f
Jeff <jeff@jeffandros.com>
2020-01-20 21:18:24 -0700

    attempt to fix the firmware size issue - disabling debug

--- keyboards/planck/keymaps/pequenininho/config.h
+++ keyboards/planck/keymaps/pequenininho/config.h
@@ -1,11 +1,13 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
+#define NO_DEBUG
+
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
+    // #define STARTUP_SONG SONG(PLANCK_SOUND)
+    #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
