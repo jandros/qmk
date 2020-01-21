3d54f2a8f4c631edda4ae7146a124c4a418b2aee
Jeff <jeff@jeffandros.com>
2020-01-20 21:59:24 -0700

    setting qwerty to default, and using non-persistent default layer changes

--- keyboards/planck/keymaps/pequenininho/config.h
+++ keyboards/planck/keymaps/pequenininho/config.h
@@ -11,7 +11,7 @@
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND)
+                                  SONG(COLEMAK_SOUND)\
                                 }
 
 #endif
