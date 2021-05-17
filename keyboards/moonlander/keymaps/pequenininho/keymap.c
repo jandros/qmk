7c7cfc3cce122a0211def62571b699def0baa829
Jeff Andros <jeff@jeffandros.com>
2021-05-17 05:04:14 +0000

    swapping space and enter keys to be more intuitive

--- keyboards/moonlander/keymaps/pequenininho/keymap.c
+++ keyboards/moonlander/keymaps/pequenininho/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
 	    KC_LCTL, KC_LGUI, KC_LALT,KC_LEFT, KC_RGHT,       DM_PLY1,               DM_PLY2,      KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            KC_SPC,  LOWER, KC_LGUI,           KC_LALT,  RAISE,  KC_ENT
+                                            KC_ENT,  LOWER, KC_LGUI,           KC_LALT,  RAISE,  KC_SPC
     ),
 
     [_MAC] = LAYOUT_moonlander(
